4fa61451bf46edb3c63a19fdcee52b9d69eb276f
gael55x <gaille.amolong1@gmail.com>
2025-07-29 14:26:02 +0800

    Added new code functions in basic directory java

---
+++ basic/hello-world/Main.java
@@ -0,0 +1,6 @@
+// name of java file must match with the main public class 
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World"); 
+    }
+}
---
+++ basic/linked-list/Linkedlist.java
@@ -0,0 +1,49 @@
+public class Linkedlist{
+    private static class Node{
+        int val; 
+        Node next; 
+
+        Node(int val) {
+            this.val = val; 
+            this.next = null; 
+        }
+    }
+
+    private Node head; 
+
+    public void insert(int val, int idx){
+        Node newN = new Node(val);
+
+        if (idx == 0){
+            newN.next = head; 
+            head = newN; 
+            return; 
+        }
+
+        Node curr = head; 
+        int c = 0; 
+
+        while (curr != null && c < idx - 1){
+            curr = curr.next; 
+            c++; 
+        }
+        // 1 -> 2 -> 3 -> 4
+        // insert: 1.2 idx: 2
+        // 1 -> 2 -> 3 -> 1.2 -> 4
+        newN.next = curr.next; 
+        curr.next = newN; 
+
+    }
+
+    public int delete(Node node){
+
+    }
+
+    public void printlist(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
